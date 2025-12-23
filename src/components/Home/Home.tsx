e64624be307d20b64cbc3593fc56efb92323588d
WadaDiriba <wedadiriba@gmail.com>
2025-12-23 16:01:03 +0900

    some home apage and other thing been good

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,26 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
   return (
+    <section id="home" className={styles.homeSection}>
+      
+      {/* Background Slider */}
+      <div className={styles.slider}>
+        <div className={`${styles.slide} ${styles.slide1}`}></div>
+        <div className={`${styles.slide} ${styles.slide2}`}></div>
+        <div className={`${styles.slide} ${styles.slide3}`}></div>
+      </div>
 
-    <div> </div>
+      {/* Overlay */}
+      <div className={styles.overlay}></div>
+
+      {/* Content */}
+      <div className={styles.homeContent}>
+        <h1>Welcome to Ethiopian Berhana Kiristos Church</h1>
+        <p>Experience the light of Christ in our community.</p>
+        <a href="#about" className={styles.btn}>Learn More</a>
+      </div>
+
+    </section>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,35 @@
-import "./Navbar.module.css";
+import styles from "./Navbar.module.css";
+import logo from "../../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <nav className="logo">
-      <img src="/logo.png" alt="Church Logo" width="80" height="80" />
-      <h3>Ethiopian Berhana Kiristos Church</h3>
+    <header className={styles.header}>
+      <nav className={styles.navbar} aria-label="Main Navigation">
+        
+        {/* Brand */}
+        <div className={styles.brand}>
+          <img src={logo} alt="Ethiopian Berhana Kiristos Church Logo" />
+          <span className={styles.brandText}>
+            Ethiopian Berhana Kiristos Church
+          </span>
+        </div>
 
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#campus">Campus</a></li>
-        <li><a href="#library">Library</a></li>
-        <li><a href="#testmonial">Testmonial</a></li>
-        <li><a href="#donate">Donate</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
+        {/* Navigation Links */}
+        <ul className={styles.navLinks}>
+          <li><a href="#home" className={styles.active}>Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#campus">Campus</a></li>
+          <li><a href="#library">Library</a></li>
+          <li><a href="#testimonial">Testimonial</a></li>
+          <li>
+            <a href="#donate" className={styles.donateBtn}>
+              Donate
+            </a>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+      </nav>
+    </header>
   );
 }
